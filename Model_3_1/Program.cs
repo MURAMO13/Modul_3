4a53f50b1527611de35e841984cddc0c052373c0
muramo13 <monbon08494@gmail.com>
2024-11-20 16:13:04 +0500

    4.3.11

--- Model_3_1/Program.cs
+++ Model_3_1/Program.cs
@@ -1,25 +1,35 @@
-﻿namespace Model_3_1;
+﻿using System.Threading.Channels;
+
+namespace Model_3_1;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Введите своё имя:");
-        var name = Console.ReadLine();
+        int[,] array = { { 1, 2, 3 }, { 5, 6, 7 }, { 8, 9, 10 }, { 11, 12, 13 } };
 
-        for (int i = name.Length - 1; i > -1 ; i--) 
+        var l1 = array.GetUpperBound(0) + 1;
+        var l2 = array.GetUpperBound(1) + 1;
+
+        for (int i = 0; i < l2; i++)
         {
-            Console.Write(name[i] + " ");
+            
+            for (int k = 0 ; k < l1; k++ )
+                Console.Write(array[k, i] + " ");
+
+            
+
+            Console.WriteLine();
         }
-
-       
-
-        Console.Write("Последная буква вашего имени:" + name[name.Length-1]);
-
-
 
 
         Console.ReadKey();
     }
+
+
+
+
+    public void cw(string some) { Console.WriteLine(some); }
+
 }
 
 
