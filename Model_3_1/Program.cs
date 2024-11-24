05fc04cec65cb8d146574c559c68e6227f8a8487
muramo13 <monbon08494@gmail.com>
2024-11-24 14:02:02 +0500

    random

--- Model_3_1/Program.cs
+++ Model_3_1/Program.cs
@@ -1,32 +1,29 @@
-﻿using System.Threading.Channels;
+﻿using System.Reflection;
+using System.Threading.Channels;
+using System.Xml.Linq;
 
 namespace Model_3_1;
 class Program
 {
     static void Main()
     {
-        int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+        //// Создаём кортеж
+        //(string Name, string Type, double Age) Pet = ("Барсик", "Кошка", 2.5);
 
+        //// Получаем тип кортежа
+        //Type tupleType = Pet.GetType();
 
-        int negative = 0, 
-            positive = 0;
+        //// Перебираем все поля кортежа
+        //foreach (var field in tupleType.GetFields(BindingFlags.Instance | BindingFlags.Public))
+        //{
+        //    // Выводим имя поля и его значение
+        //    Console.WriteLine($"{field.Name}: {field.GetValue(Pet)}");
+        //}
 
-        foreach (var i in arr)
-        {
-            
-            if ( i > 0) 
-            {
-                positive++;
-            }
-            else if (i < 0)
-            {
-                negative++;
-            }
-        }
-        Console.WriteLine(negative);
-        Console.WriteLine(positive);
-       
-
+        Random random = new Random();
+        byte[] buffer = new byte[5];
+        random.NextBytes(buffer); // Заполнит массив случайными байтами
+        Console.WriteLine(string.Join(", ", buffer));
 
 
 
