fef8d61b9d7dce0455622901bac0842477f95be0
muramo13 <monbon08494@gmail.com>
2024-12-08 10:07:30 +0500

    Итоговый проект 5.6.1 (HW-03)

--- Model_3_1/Program.cs
+++ Model_3_1/Program.cs
@@ -1,32 +1,122 @@
-﻿using System.Reflection;
-using System.Threading.Channels;
-using System.Xml.Linq;
+﻿
 
 namespace Model_3_1;
 class Program
 {
     static void Main()
     {
-        Console.WriteLine(" Enter somethring ");
-        var EchoText = Console.ReadLine();
 
-        var DepthInt = EchoText.Length;
+        var User = GetInfoUser();
 
-        EchoTextMethod(EchoText, DepthInt);
+        WatchUser(User);
 
 
 
         Console.ReadKey();
     }
-
-    static void EchoTextMethod(string text, int depht)
+    static (string FirstName, string LastName, int age, bool HasPet, string[] PetsArrName, string[] FavArrCol) GetInfoUser()
     {
-        if (depht > 0)
+
+        (string FirstName, string LastName, int age, bool HasPet, string[] PetsArrName, string[] FavArrCol) user;
+        Console.WriteLine("What is your name?");
+        user.FirstName = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?");
+        user.LastName = Console.ReadLine();
+
+        Console.WriteLine("How old are you?");
+        user.age = CorrectInt();
+
+        Console.WriteLine("Do you have pets? \n Yes or No.");
+        user.HasPet = CorectBool();
+        if (user.HasPet)
         {
-            var int6 = (depht % 16);
-            Console.BackgroundColor = (ConsoleColor)(int6);
-            Console.WriteLine("  >>>" + text);
-            EchoTextMethod(text.Remove(0, 1), depht - 1);
+            Console.WriteLine("How many pets do you have?");
+            user.PetsArrName = FillArrs(new string[CorrectInt()]);
+        }
+        else
+        {
+            user.PetsArrName = Array.Empty<string>();
+        }
+
+        Console.WriteLine("How many favorite colors do you have?");
+        user.FavArrCol = FillArrs(new string[CorrectInt()]);
+
+        return user;
+    }
+
+    static bool CorectBool()
+    {
+        string input = Console.ReadLine().ToLower();
+        if (input == "yes")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    static int CorrectInt()
+    {
+        int NaturelNum;
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (int.TryParse(input, out NaturelNum) && NaturelNum > 0)
+            {
+                return NaturelNum;
+            }
+            else
+            {
+                Console.WriteLine("Try again! \n And enter correct number");
+            }
+        }
+
+    }
+
+    static string[] FillArrs(string[] arrToFill)
+    {
+        for (int i = 0; i < arrToFill.Length; ++i)
+        {
+            Console.WriteLine($"Enter {i+1} item: ");
+            arrToFill[i] = Console.ReadLine();
+        }
+        return arrToFill;
+    }
+
+    static void WatchUser((string FirstName, string LastName, int age, bool HasPet, string[] PetsArrName, string[] FavArrCol) user) 
+    {
+        Console.WriteLine($"User name: {user.FirstName}");
+        Console.WriteLine($"User last name:{user.LastName}");
+        Console.WriteLine($"User age:{user.age}");
+      
+        if (user.HasPet && user.PetsArrName.Length > 0)
+        {
+            Console.WriteLine("Pets: ");
+            foreach (var item in user.PetsArrName)
+            {
+                Console.WriteLine($"\t {item}");
+            }
+        }
+        else 
+        {
+            Console.WriteLine("You have not pets!");
+        }
+
+        if (user.FavArrCol.Length > 0)
+        {
+            Console.WriteLine("Your favorite colors");
+            foreach (var item in user.FavArrCol)
+            {
+                Console.WriteLine($"\t {item}");
+            }
+        }
+        else
+        {
+            Console.WriteLine("The user has not listed any favorite colors.");
         }
     }
 
