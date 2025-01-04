466ad10295cd9a690825750797f0df0a7839b094
muramo13 <monbon08494@gmail.com>
2025-01-04 23:56:53 +0500

    7.7

--- Model_3_1/Program.cs
+++ Model_3_1/Program.cs
@@ -1,116 +1,181 @@
-﻿namespace Model_3_1;
-class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Model_3_1
 {
-    static void Main()
+    public enum CourierCompany
     {
-        while (true)
+        DHL,
+        AmazonPrime,
+        ExpressPony
+    }
+    public enum DeliveryType
+    {
+        OneDay,
+        Usual
+    }
+
+    public static class Extensions
+    {
+        public static bool IsExpress(this DeliveryType deliveryType)
         {
-            var temp =int.Parse( Console.ReadLine() );
-            Console.WriteLine( temp.GetNegative() );
-            Console.WriteLine( temp.GetPositive() );
+            return DeliveryType.OneDay == deliveryType;
         }
+    }
+
+    abstract class Delivery 
+    {
+        public static int TotalDelivery = 0;
+        public Basket<Article> Basket { get; private set; }
+        public CourierCompany Company { get; private set; }
+        public DeliveryType DeliveryType { get; private set; }
+        public string Address { get; private set; }
+
+        protected Delivery( Basket<Article> basket, string address, CourierCompany company, DeliveryType deliveryType)
+        {
+            Basket = basket;
+            Address = address;
+            Company = company;
+            DeliveryType = deliveryType;
+            ++TotalDelivery;
+        }
+
        
+
+        public abstract void ShowInfoDelivery();
+
     }
-
-}
-
-class BaseClass
-{
-    protected string Name;
-
-    public BaseClass(string name) 
+    class HomeDelivery : Delivery
     {
-        Name = name;
-    }
+        public DateTime DeliveryTime;
 
-   protected virtual void Display()
-   {
-        Console.WriteLine("Hello from BaseClass");  
-   }
-
-    protected virtual int Counter { get; set; }
-}
-
-class DerivedClass : BaseClass
-{
-    public string Description;
-
-    public int counter;
-    public DerivedClass (string name, string description) : base(name)
-    {
-        Description = description;
-    }
-
-    public DerivedClass(string name, string description, int counter) : this(name, description)
-    {
-        Counter = counter;
-    }
-
-    protected override void Display()
-    {
-        Console.WriteLine("Hello from DerivedClass");
-    }
-
-    
-    protected override int Counter 
-    { 
-        get => counter ;
-        set 
+        public HomeDelivery(DateTime deliveryTime, Basket<Article> basket, string address, CourierCompany company, DeliveryType deliveryType) : base(basket, address, company, deliveryType)
         {
-            if (value >= 0)
+            DeliveryTime = deliveryTime;
+            
+        }
+
+        public override void ShowInfoDelivery()
+        {
+            Console.WriteLine( $"Home Delivery to {Address} at {DeliveryTime}.\n " +
+                $"Busket:{Basket.GetArticles()}\n" +
+                $"Delivery company:{Company}\n" +
+                $"Delivery type: {DeliveryType} " +
+                $"Price:{Basket.GetTotalPrice()}" );
+        }
+    }
+    class ShopDelivery : Delivery
+    {
+        public DateTime PickUpTime;
+
+        public ShopDelivery(DateTime pickUpTime, Basket<Article> basket, string address, CourierCompany company, DeliveryType deliveryType) : base(basket, address, company, deliveryType)
+        {
+            PickUpTime = pickUpTime;
+        }
+        public override void ShowInfoDelivery()
+        {
+            Console.WriteLine($"Shop Delivery to {Address} at {PickUpTime}.\n " +
+                $"Busket:{Basket.GetArticles()}\n" +
+                $"Delivery company:{Company}\n" +
+                $"Delivery type: {DeliveryType} " +
+                $"Price:{Basket.GetTotalPrice()}");
+        }
+    }
+    class PickPointDelivery : Delivery
+    {
+        public DateTime PickUpTime;
+
+        public PickPointDelivery(DateTime pickUpTime, Basket<Article> basket, string address, CourierCompany company, DeliveryType deliveryType) : base(basket, address, company, deliveryType)
+        {
+            PickUpTime = pickUpTime;
+        }
+        public override void ShowInfoDelivery()
+        {
+            Console.WriteLine($"Shop Delivery to {Address} at {PickUpTime}.\n " +
+                $"Busket:{Basket.GetArticles()}\n" +
+                $"Delivery company:{Company}\n" +
+                $"Delivery type: {DeliveryType}" +
+                $"Price:{Basket.GetTotalPrice()}");
+        }
+    }
+    class Article 
+    {
+        public string Title { get; private set; }
+        public int Id { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Article(string Title, int Id, decimal Price) 
+        {
+            this.Title = Title;
+            this.Id = Id;
+            this.Price = Price;
+        }
+    }
+    class OfflineArticle : Article
+    {
+        private string ShopAddress { get; set; }
+        public OfflineArticle(string Title, int Id, decimal Price, string shopAddress) : base(Title, Id, Price)
+        {
+            ShopAddress = shopAddress;
+        }
+    }
+    class Basket<TArticle>
+    {
+        private static int _basketCounter = 0;
+        public int Id { get; private set; }
+
+        private List<TArticle> _articles = new();
+
+        public void AddArticle(TArticle article)
+        {
+            _articles.Add(article);
+        }
+
+        public string GetArticles()
+        {
+            return string.Join(", ", _articles.Select(a => a.Title));
+        }
+
+        public decimal GetTotalPrice()
+        {
+            decimal total = 0;
+
+            foreach (TArticle article in _articles)
             {
-                Counter = value;
-
+                total += article.Price;
             }
-            else { Console.WriteLine(" U input wrong int"); }
-        } 
-    }
-}
+            return total;
 
-class Ob 
-{
-    public int Value;
-
-    public static Ob operator + (Ob a, Ob b) 
-    {
-        return new Ob {Value = a.Value + b.Value };
-    }
-
-}
-
-class HelloIndex
-{
-    public int[] arr;
-    public HelloIndex(int[] arr)
-    {
-        this.arr = arr;
-    }
-
-    public int this[int v] 
-    {
-        get
-        {
-            return arr[v];
         }
-        set 
+
+        public Basket()
         {
-            arr[v] = value;
+            Id = ++_basketCounter;
         }
-    }
-}
 
-static class newint
-{
-    public static int GetNegative(this int source) 
-    {
-        return source <= 0 ? source  : -source;
+        public TArticle this[int index]
+        {
+            get { return _articles[index]; }
+            set { _articles[index] = value; }
+        }
 
     }
 
-    public static int GetPositive(this int source)
+    class OfflineBasket<TOfflineArticle> : Basket<TOfflineArticle> where TOfflineArticle : OfflineArticle
     {
-        return source >= 0 ? source : -source;
+        
+    }
 
+
+
+
+    class Program
+    {
+        static void Main()
+        {
+          
+        }
     }
 }
 
