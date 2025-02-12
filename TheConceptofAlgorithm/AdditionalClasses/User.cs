ca1b65efa05b51801782d5a62af8a3480a37e3ca
muramo13 <monbon08494@gmail.com>
2025-02-12 23:38:33 +0500

    12.1. Понятие алгоритма

---
+++ TheConceptofAlgorithm/AdditionalClasses/User.cs
@@ -0,0 +1,8 @@
+﻿namespace TheConceptofAlgorithm.AdditionalClasses;
+
+internal class User
+{
+    public string? Login { get; set; }
+    public string? Name { get; set; }
+    public bool IsPremium { get; set; }
+}
---
+++ TheConceptofAlgorithm/Program.cs
@@ -0,0 +1,37 @@
+﻿using TheConceptofAlgorithm.AdditionalClasses;
+namespace TheConceptofAlgorithm;
+class Program
+{
+    static void Main()
+    {
+        List<User> users = new List<User>
+        {
+            new User { Login = "user1", Name = "Иван", IsPremium = true },
+            new User { Login = "user2", Name = "Анна", IsPremium = false },
+            new User { Login = "user3", Name = "Макс", IsPremium = true },
+            new User { Login = "user4", Name = "Мария", IsPremium = false }
+        };
+
+        foreach (var user in users)
+        {
+            Console.WriteLine($"Здравствуйте, {user.Name}!");
+
+            if (!user.IsPremium)
+            {
+                ShowAds();
+            }
+        }
+    }
+
+    static void ShowAds()
+    {
+        Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+        Thread.Sleep(1000);
+
+        Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+        Thread.Sleep(3000);
+    }
+}
